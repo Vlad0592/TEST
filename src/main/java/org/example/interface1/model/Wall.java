d738f28d7688d4de593e7630757c45d30edd4e35
vlad <vladislav.nesterov1990@mail.ru>
2023-04-14 12:49:47 +0300

    Done 'interface'

---
+++ src/main/java/org/example/interface1/model/Wall.java
@@ -0,0 +1,12 @@
+package org.example.interface1.model;
+
+public class Wall {
+    private Double height;
+    public Wall(Double height) {
+        this.height = height;
+    }
+    //Возвращает высоту стенки
+    public Double getHeight() {
+        return height;
+    }
+}
