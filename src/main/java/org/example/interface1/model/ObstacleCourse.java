22bfb04a4650e143a7f6e8246d8005301531ed61
vlad <vladislav.nesterov1990@mail.ru>
2023-04-14 12:49:34 +0300

    Done 'interface'

---
+++ src/main/java/org/example/interface1/model/ObstacleCourse.java
@@ -0,0 +1,19 @@
+package org.example.interface1.model;
+
+import java.util.ArrayList;
+
+public class ObstacleCourse {
+
+    private ArrayList<Obstacle> obstaclecourse;
+
+    public ObstacleCourse() {
+        obstaclecourse=new ArrayList<Obstacle>();
+    }
+    // Добавляет препятствие из дорожки и стены
+    public void obstacleAdd(Obstacle obstacle){
+        obstaclecourse.add(obstacle);
+    }
+    // Возвращает набор препятствий
+    public  ArrayList<Obstacle> getObstacles(){ return obstaclecourse;}
+
+}
